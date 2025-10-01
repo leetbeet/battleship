90ace559b328649e3e4b23811ddef1313e6c9261
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-10-01 13:00:16 +0800

    feat: add boards to the UI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import "./style.css";
+import './style.css';
+
+// create boards
+(() => {
+  const board1 = document.querySelector('.board1');
+  const board2 = document.querySelector('.board2');
+
+  for (let i = 0; i < 100; i++) {
+    const cell1 = document.createElement('div');
+    const cell2 = document.createElement('div');
+    board1.appendChild(cell1);
+    board2.appendChild(cell2);
+  }
+})();
