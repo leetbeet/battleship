8ff00af3b9a191f20ab51476fd1f053eb376fbc6
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-10-01 13:01:17 +0800

    feat: add board rendering and event listeners for hit detection

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import { Player, Computer } from './classes/player';
 
 // create boards
 (() => {
@@ -12,3 +13,49 @@ import './style.css';
     board2.appendChild(cell2);
   }
 })();
+
+const initBoards = (name) => {
+  const board1 = document.querySelector('.board1');
+  const board2 = document.querySelector('.board2');
+  const player1 = new Player(name);
+  const player2 = new Computer();
+  player1.gameboard.placeAllRandomly();
+  player2.gameboard.placeAllRandomly();
+
+  const cells1 = board1.children;
+  const cells2 = board2.children;
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const k = i + j * 10;
+      cells2[k].addEventListener('click', () => {
+        player1.attack(player2, i, j);
+        reRenderBoard(player2, board2);
+      });
+
+      if (player1.gameboard.board[i][j] !== 'empty')
+        cells1[k].className = 'ship';
+    }
+  }
+};
+
+const reRenderBoard = (player, board, isOpponent = true) => {
+  const cells = board.children;
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const k = i + j * 10;
+      switch (player.gameboard.board[i][j]) {
+        case 'miss':
+          cells[k].className = 'miss';
+          break;
+        case 'ship':
+          if (!isOpponent) cells[k].className = 'ship';
+          break;
+        case 'hit':
+          cells[k].className = 'hit';
+      }
+    }
+  }
+};
+
+initBoards('leetbeet');
