d5535d7f2f61e8575f70424cee219d5d1427e2f9
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-10-04 14:30:18 +0800

    feat: add dialogs to let players choose mode

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import './style.css';
 import { GameController } from './classes/gameController';
 
-const game = new GameController(
-  document.querySelector('.board1'),
-  document.querySelector('.board2'),
-  'leetbeet',
-  'tdab'
-);
+window.addEventListener('DOMContentLoaded', () => {
+  const dialog = document.getElementById('choose-mode');
+  dialog.showModal();
 
-game.createBoards();
-game.initBoards();
+  document.getElementById('computer-btn').addEventListener('click', () => {
+    dialog.close();
+    dialog.style.display = 'none';
+
+    const dialogNameInput = document.getElementById('input-player1-only');
+    dialogNameInput.showModal();
+    const form = document.getElementById('input-player1-only-form');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const name = document.querySelector('#name').value;
+      const game = new GameController(
+        document.querySelector('.board1'),
+        document.querySelector('.board2'),
+        name
+      );
+      dialogNameInput.close();
+      game.createBoards();
+      game.initBoards();
+      document.querySelector('.board1').style.border = '1px solid black';
+      document.querySelector('.board2').style.border = '1px solid black';
+    });
+  });
+
+  document.getElementById('2-player-btn').addEventListener('click', () => {
+    dialog.close();
+    dialog.style.display = 'none';
+
+    const dialogNameInput = document.getElementById('input-players');
+    dialogNameInput.showModal();
+    const form = document.getElementById('input-players-form');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const name1 = document.querySelector('#name1').value;
+      const name2 = document.querySelector('#name2').value;
+      const game = new GameController(
+        document.querySelector('.board1'),
+        document.querySelector('.board2'),
+        name1,
+        name2
+      );
+      dialogNameInput.close();
+      game.createBoards();
+      game.initBoards();
+      document.querySelector('.board1').style.border = '1px solid black';
+      document.querySelector('.board2').style.border = '1px solid black';
+    });
+  });
+});
